1b5e9a1d8643f2a62e27813ae83dc77aaff8a317
Marcos Ferreira <marcos.felipe.gf@gmail.com>
2022-02-18 22:16:19 -0300

    Ajusta google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,8 @@ import GlobalStyle from "../styles/global";
 import theme from "../styles/theme";
 import "bootstrap/dist/css/bootstrap.css"; // Add this line
 
+import * as gtag from "../utils/gtag";
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const router = useRouter();
 
@@ -23,12 +25,33 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
       });
   }, [router.events]);
 
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init("270116435238426"); // facebookPixelId
+        ReactPixel.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider theme={theme}>
-      
       <Component {...pageProps} />
       <GlobalStyle />
-     
     </ThemeProvider>
   );
 };
