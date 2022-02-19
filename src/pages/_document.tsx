7572fce2bfc226c2ab0fe10e4f09d8c0b5cfd5d9
Marcos Ferreira <marcos.felipe.gf@gmail.com>
2022-02-18 22:04:13 -0300

    Ajusta de analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ import Document, {
   NextScript,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import Script from "next/script";
 
 export default class MyDocument extends Document {
   static async getInitialProps(
@@ -90,6 +91,46 @@ export default class MyDocument extends Document {
             name="facebook-domain-verification"
             content="6qg0yglf52xfiz41taudnph3l0futv"
           />
+          <Script>
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-CZ2DX2MKH6"
+            ></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CZ2DX2MKH6', { page_path: window.location.pathname });
+            `,
+              }}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `!function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '270116435238426');
+                fbq('track', 'PageView');
+                `,
+              }}
+            />
+            \
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src="https://www.facebook.com/tr?id=270116435238426&ev=PageView&noscript=1"
+              />
+            </noscript>
+          </Script>
         </Head>
         <body>
           <Main />
